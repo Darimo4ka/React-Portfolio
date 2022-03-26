3c7b3fb2c341eaa4386c83ffcbe1674fccf83e72
Darimo4ka <darima_badm@yahoo.com>
2022-03-26 09:57:11 -0400

    contact  form

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./footer.css";
 
-const Footer = () => (
+const Footer = () => {
+  return(
   <div className="footer">
     <div class="col-lg-4 mb-5 mb-lg-0">
-    <a href="https://www.linkedin.com/in/darimabadmatsyrenova/"><i class="./public/assets/linkedin.png"></i></a>
-    <a href="https://github.com/Darimo4ka">github</a>
+    <a href="https://www.linkedin.com/in/darimabadmatsyrenova/">linkedIn</a>
+    
+    <a href="https://github.com/Darimo4ka">GitHub</a>
+
     <a href="https://www.instagram.com/darimachoy/">Instagram</a>
     </div>
   </div>
 );
+  }
+
+/*(Maybe use this tag to create pics so will see in the future)
+ <a href="default.asp">
+<img src="smiley.gif" alt="HTML tutorial" style="width:42px;height:42px;">
+</a> */
 
 export default Footer;
