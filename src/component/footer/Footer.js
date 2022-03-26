d4d4766855495e9db80ccc750253a707fdf99ed6
Darimo4ka <darima_badm@yahoo.com>
2022-03-26 10:09:23 -0400

    Icons work in footer

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,18 +1,28 @@
 import React from "react";
 import "./footer.css";
+// import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 const Footer = () => {
-  return(
-  <div className="footer">
-    <div class="col-lg-4 mb-5 mb-lg-0">
-    <a href="https://www.linkedin.com/in/darimabadmatsyrenova/">linkedIn</a>
-    
-    <a href="https://github.com/Darimo4ka">GitHub</a>
+  return (
+    <div className="footer">
+      <div class="col-lg-4 mb-5 mb-lg-0">
+        <a href="https://www.linkedin.com/in/darimabadmatsyrenova/">
+          <LinkedInIcon />
+        </a>
 
-    <a href="https://www.instagram.com/darimachoy/">Instagram</a>
+        <a href="https://github.com/Darimo4ka">
+          <GitHubIcon />
+        </a>
+
+        <a href="https://www.instagram.com/darimachoy/">
+          <InstagramIcon />
+        </a>
+      </div>
     </div>
-  </div>
-);
+  );
   }
 
 /*(Maybe use this tag to create pics so will see in the future)
